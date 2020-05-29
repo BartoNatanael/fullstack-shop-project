dcf19266580e2480270b7297fb03de79c2731f51
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-05-29 12:46:32 +0200

    something is not yes

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -3,64 +3,61 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { signin } from '../actions/userActions';
 
-
 function SigninScreen(props) {
 
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const userSignin = useSelector(state=>state.userSignin);
+  const [password, setPassword] = useState('');
+  const userSignin = useSelector(state => state.userSignin);
   const { loading, userInfo, error } = userSignin;
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-
-    return () => {
+  useEffect(() => {
+    if (userInfo) {
       props.history.push("/");
+    }
+    return () => {
+      //
     };
-  }, [userInfo])
+  }, [userInfo]);
 
   const submitHandler = (e) => {
-      e.preventDefault();
-      dispatch(signin(email, password))
-  }
+    e.preventDefault();
+    dispatch(signin(email, password));
 
+  }
   return <div className="form">
-      <form onSubmit={submitHandler}>
-          <ul className="form-container">
-            <li>
-                <h2>
-                    Sing In
-                </h2>
-            </li>
-            <li>
-                {loading && <div>Loading...</div>}
-                {error && <div>{error}</div>}
-            </li>
-            <li>
-                <label for="email">
-                    Email
-                </label>
-                <input type="email" name="email" id="email" onChange={(e)=>setEmail(e.target.value)}>
-                </input>
-            </li>
-            <li>
-                <label for="password">
-                    Password    
-                </label>
-                <input type="password" id="password" name="password" onChange={(e)=>setPassword(e.target.value)}>
-                </input>    
-            </li>
-            <li>
-                <button type="submit" className="button primary">Sing In</button>
-            </li>
-            <li>
-                New to amazona?
-            </li>
-            <li>
-                <Link to="/register" className="button text-center secondary">Create your amazona account</Link>
-            </li>
-          </ul>
-      </form>
+    <form onSubmit={submitHandler} >
+      <ul className="form-container">
+        <li>
+          <h2>Sign-In</h2>
+        </li>
+        <li>
+          {loading && <div>Loading...</div>}
+          {error && <div>{error}</div>}
+        </li>
+        <li>
+          <label htmlFor="email">
+            Email
+          </label>
+          <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}>
+          </input>
+        </li>
+        <li>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}>
+          </input>
+        </li>
+        <li>
+          <button type="submit" className="button primary">Signin</button>
+        </li>
+        <li>
+          New to amazona?
+        </li>
+        <li>
+          <Link to="/register" className="button secondary text-center" >Create your amazona account</Link>
+        </li>
+      </ul>
+    </form>
   </div>
 }
 export default SigninScreen;
